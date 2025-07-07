b487daf3304ab9fd50448a5009a36cd892f42f92
naveenmathramkott <naveenmathramkott@gmail.com>
2025-07-07 21:19:59 +0400

    server path update

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: async (req, file, cb) => {
-    const uploadDir = path.join(__dirname, "./processedImages");
+    const uploadDir = path.join(process.cwd(), "processedImages");
     await makeDirIfNotExists(uploadDir);
     cb(null, uploadDir);
   },
